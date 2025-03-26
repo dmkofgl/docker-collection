54943bb5fad6a7dd4f255c34536b7277a6f5af33
uladzislau.barkou <uladzislau.barkou@altruist.com>
2025-03-26 11:38:06 +0100

    add examples

--- examples/copy/run.sh
+++ examples/copy/run.sh
@@ -1,3 +1,6 @@
 docker build -t example-copy .
 
 docker run --rm example-copy
+
+docker build -t example-copy-github  https://github.com/dmkofgl/docker-collection/
+docker run --rm example-copy-github
---
+++ examples/remote-build-context/run-remote-build.sh
@@ -0,0 +1,2 @@
+
+docker build -t example-copy-github  https://github.com/dmkofgl/docker-collection/tree/master

ab5bdad4ca4749b556f5da9432ea71fb9fe0ff48
uladzislau.barkou <uladzislau.barkou@altruist.com>
2025-03-26 16:19:23 +0100

    add examples

--- examples/cli-basics/volume/run.sh
+++ examples/cli-basics/volume/run.sh
@@ -1,10 +1,21 @@
-
-#docker volume create hello-vol
-##If you specify a volume name already in use on the current driver, Docker assumes you want to reuse the existing volume and doesn't return an error.
-#docker volume create hello-vol
+volumeName=hello-vol
+docker volume create $volumeName
+#If you specify a volume name already in use on the current driver, Docker assumes you want to reuse the existing volume and doesn't return an error.
+docker volume create $volumeName
 
 docker build -t testing .
-#docker run --rm -v hello-vol:/base testing
-#docker run --rm -it --name test-vol -v hello-vol:/world ubuntu ls -l world/
+docker run --rm -v $volumeName:/base testing
+docker run --rm -it --name test-vol -v $volumeName:/world ubuntu ls -lR world/
 
+docker volume inspect $volumeName
 
+docker volume ls
+docker volume create not-used
+
+docker volume prune
+
+docker volume create not-used-again
+
+docker volume rm not-used-again
+#Can't remove used volume
+docker volume rm $volumeName
